2cf51b859873e1df33d295730f526a9c8f25d828
Christian Simon <csimon1127@gmail.com>
2020-09-23 23:30:29 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+
+class School 
+  attr_reader :roster
+  
+  def initialize(roster)
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+  
+  def grade(number)
+    @roster[number]
+  end
+  
+  def sort
+    @roster.map { |grade, people|
+      people.sort!
+    }
+    @roster
+  end
+  
+end 
